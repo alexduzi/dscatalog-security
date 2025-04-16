a95a99bb985070b902a5070037e1480be3576134
alexduzi <duzihd@gmail.com>
2025-04-15 21:11:33 -0300

    User/Role relation

--- src/main/java/com/devsuperior/dscatalog/entities/Role.java
+++ src/main/java/com/devsuperior/dscatalog/entities/Role.java
@@ -1,12 +1,23 @@
 package com.devsuperior.dscatalog.entities;
 
-import java.io.Serializable;
-import java.util.Objects;
+import jakarta.persistence.*;
 
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_role")
 public class Role implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String authority;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users = new HashSet<>();
 
     public Role() {
     }
@@ -30,6 +41,10 @@ public class Role implements Serializable {
 
     public void setAuthority(String authority) {
         this.authority = authority;
+    }
+
+    public Set<User> getUsers() {
+        return users;
     }
 
     @Override
