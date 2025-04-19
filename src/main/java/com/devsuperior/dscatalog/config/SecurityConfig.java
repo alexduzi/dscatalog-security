d327ee4b322d416a09680f140d7c41a8d6e7261a
alexduzi <duzihd@gmail.com>
2025-04-18 21:20:31 -0300

    Users resource/controller

--- src/main/java/com/devsuperior/dscatalog/config/SecurityConfig.java
+++ src/main/java/com/devsuperior/dscatalog/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.devsuperior.dscatalog.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -10,8 +12,8 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-        http.csrf(csrf -> csrf.disable());
+        http.csrf(AbstractHttpConfigurer::disable)
+                .headers(httpSecurityHeadersConfigurer -> httpSecurityHeadersConfigurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
         http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
         return http.build();
     }
