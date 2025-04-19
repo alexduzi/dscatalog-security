aca07bc3db1bcf8d4d2d100fdcdb56068dd89142
alexduzi <duzihd@gmail.com>
2025-04-19 19:30:59 -0300

    User update validator

---
+++ src/main/java/com/devsuperior/dscatalog/services/validation/UserUpdateValidator.java
@@ -0,0 +1,54 @@
+package com.devsuperior.dscatalog.services.validation;
+
+import com.devsuperior.dscatalog.dto.UserUpdateDTO;
+import com.devsuperior.dscatalog.entities.User;
+import com.devsuperior.dscatalog.repositories.UserRepository;
+import com.devsuperior.dscatalog.resources.exceptions.FieldMessage;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import org.springframework.web.servlet.HandlerMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class UserUpdateValidator implements ConstraintValidator<UserUpdateValid, UserUpdateDTO> {
+
+    private final UserRepository repository;
+
+    private final HttpServletRequest request;
+
+    public UserUpdateValidator(UserRepository repository, HttpServletRequest request) {
+        this.repository = repository;
+        this.request = request;
+    }
+
+    @Override
+    public void initialize(UserUpdateValid constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(UserUpdateDTO value, ConstraintValidatorContext context) {
+        List<FieldMessage> list = new ArrayList<>();
+
+        @SuppressWarnings("unchecked")
+        var uriVars = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+        long userId = Long.parseLong(uriVars.get("id"));
+
+        Optional<User> user = repository.findByEmail(value.getEmail());
+
+        if (user.isPresent() && user.get().getId() != userId) {
+            list.add(new FieldMessage("email", "Email já existente"));
+        }
+
+        for (FieldMessage e : list) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName()).addConstraintViolation();
+        }
+
+        return list.isEmpty();
+    }
+}
